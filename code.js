b5ca76f4ccad39245f11eb59deb7a4c0bf357d8c
RebornEnder <120225426+RebornEnder@users.noreply.github.com>
2023-11-05 13:27:16 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,18 @@
+const changeFavicon = link => {
+  let $favicon = document.querySelector('link[rel="icon"]')
+  // If a <link rel="icon"> element already exists,
+  // change its href to the given link.
+  if ($favicon !== null) {
+    $favicon.href = link
+  // Otherwise, create a new element and append it to <head>.
+  } else {
+    $favicon = document.createElement("link")
+    $favicon.rel = "icon"
+    $favicon.href = link
+    document.head.appendChild($favicon)
+  }
+}
+
+e = prompt("Enter URL To Spoof, ex: "+window.location.host)
+document.title = e
+changeFavicon('https://'+e+'/favicon.ico')
